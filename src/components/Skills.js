c604246f87b01ae67b87b27d898396b6af65af6f
hpekkan <hpekkan@etu.edu.tr>
2023-02-11 23:19:33 +0300

    codex description and contact update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,25 +34,24 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>About Skills</h2>
-                        <p>I have learned different programming languages ​​so far and as far as my skills are concerned<br></br></p>
+                        <h2>What is Cosmic Codex?</h2>
+                        <p>At Cosmic Codex, I've decided to showcase my abilities in a more direct and transparent way by highlighting my tangible projects in
+                         the fields of AI, ML, and web development. Instead of relying on percentage-based skill charts, my portfolio provides a clear representation
+                          of my capabilities through the projects I've worked on, ensuring that what you see is a genuine and accurate portrayal of my skills.<br></br></p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>English</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
+                                
                                 <h5>Web Development</h5>
                             </div>
+                            <div className="item">
+                                
+                                <h5>Web Development</h5>
+                            </div>
+                            
                         </Carousel>
                     </div>
                 </div>
